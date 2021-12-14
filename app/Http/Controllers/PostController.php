a05b7c6a59c2c5c872356375ef1928c32db81ea3
amarGhandour <amarghandour89@gmail.com>
2021-12-14 04:24:45 +0200

    make pagination links

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,9 +8,8 @@ class PostController extends Controller
 {
     public function index()
     {
-
         return view('posts.index', [
-            'posts' => Post::latest()->filter(request(['search', 'category', 'user']))->get(),
+            'posts' => Post::latest()->filter(request(['search', 'category', 'user']))->paginate(6)->withQueryString()
         ]);
     }
 
