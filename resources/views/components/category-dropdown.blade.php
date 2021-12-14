5027aabc7841ca07d99772a1ce06af27c563234f
amarGhandour <amarghandour89@gmail.com>
2021-12-14 04:28:17 +0200

    make pagination links

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -13,13 +13,14 @@
             </button>
         </x-slot>
 
-        <x-dropdown-item href="/" :active="request()->routeIs('home') && request('category') === null">
+        <x-dropdown-item href="/?{{http_build_query(request()->except('$category', 'page'))}}"
+                         :active="request()->routeIs('home') && request('category') === null">
             All
         </x-dropdown-item>
 
         @foreach($categories as $category)
             <x-dropdown-item
-                href="?category={{$category->slug}}&{{http_build_query(request()->except('$category'))}}"
+                href="/?category={{$category->slug}}&{{http_build_query(request()->except('category', 'page'))}}"
                 :active="isset($currentCategory) && $category->is($currentCategory)"
             >{{ucwords($category->name)}}
             </x-dropdown-item>
